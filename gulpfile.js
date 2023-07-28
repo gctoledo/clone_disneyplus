13f7de8e79a4e762b76c160537a528fa675374c3
Gabriel Costa Toledo <gabrielcostatoledo@outlook.com>
2023-07-28 00:28:15 -0300

    add hero section and fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp = require('gulp'); //importa o gulp
+const sass = require('gulp-sass')(require('sass')); //importa sass e gulp-sass
+const strip = require('gulp-strip-comments'); //importa plugin
+const imagemin = require('gulp-imagemin');
 
-function styles() {
+function styles() { //transforma scss em css e comprime o arquivo
     return gulp.src('./src/styles/*.scss')
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
+function images() { //minifica as imagens
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+function removeComments() { //remove os comentarios
+    return gulp.src('./comments/scripts/*.js')
+    .pipe(strip())
+    .pipe(gulp.dest('./src/scripts'));
+}
+
+exports.default = gulp.parallel(styles, images, removeComments);
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
 }
